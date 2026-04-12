49b93861df834b9d5b171397feeb04813bab45b0
Pushkaraj Baradkar <baradkar@usc.edu>
2026-04-11 23:43:24 -0700

    commit

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,6 @@ import createMDX from "@next/mdx";
 const nextConfig: NextConfig = {
   pageExtensions: ["ts", "tsx", "md", "mdx"],
   transpilePackages: ["three"],
-  async rewrites() {
-    return [
-      {
-        source: "/favicon.ico",
-        destination: "/favicon.svg",
-      },
-    ];
-  },
 };
 
 const withMDX = createMDX({});
